3104cb2a78e5677d84809257101340ef45a0c5f1
Dorian Buck <dorianbuck@gmail.com>
2021-10-10 21:03:40 +0200

    adds test for login feature

--- cypress/integration/userCanLogin.feature.js
+++ cypress/integration/userCanLogin.feature.js
@@ -1,13 +1,27 @@
-describe('User can enter user data and login', () => {
-  
+describe("User can enter user data and login", () => {
   beforeEach(() => {
     cy.intercept("**api/auth/**", {
-      fixture: "userRegistrationResponse.json",
-    });
+      fixture: "loginFixture.json",
+    }).as("loginResponse");
     cy.visit("/");
     cy.get("[data-cy=login-btn]").click();
   });
 
+  it("is expected to have two input fields and a submit button", () => {
+    cy.get("form").children().should("have.length", 3);
+  });
 
-
+  describe("", () => {
+    before(() => {
+      cy.get("[data-cy=email-input]").type("user@email.com");
+      cy.get("[data-cy=password-input]").type("password");
+      cy.get("[data-cy=submission-login]").click({ multiple: true });
+    });
+    it("is expected that the response includes a success status", () => {
+      cy.wait("@loginResponse").its("response.statusCode").should("eq", 200);
+    });
+    it("is expected to bring you to the Menu Page", () => {
+      cy.get("[data-cy=starter-tab]").should("be.visible");
+    });
+  });
 });
